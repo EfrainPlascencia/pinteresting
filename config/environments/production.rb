16e6c93cec1d91bac3bd3e6474348a9438eeb3b0
Efrain Plascencia <efrain.plascencia@gmail.com>
2014-09-01 19:33:29 -0700

    Fix: ArgumentError (misswing required :bucket option) v3.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,11 +83,11 @@ Rails.application.configure do
   # Required for Heroku
   config.action_mailer.default_url_options = { :host => 'ep-pinteresting.herokuapp.com' }
 
-  # Sets Paperclip to upload images to Amazon S3
+  # Sets Paperclip to upload images to Amazon
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
+    :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
