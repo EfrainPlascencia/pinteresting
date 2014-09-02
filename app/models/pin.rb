1532df0dcffd9a03a483e18bd3eed1424b44935e
Efrain Plascencia <efrain.plascencia@gmail.com>
2014-09-01 19:25:17 -0700

    Fix: ArgumentError (misswing required :bucket option)

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,8 +1,12 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+	has_attached_file :image, 
+					  :styles => { :medium => "300x300>", :thumb => "100x100>" },
+					  :storage => :s3,
+					  :bucket => ENV[S3_BUCKET_NAME]
+	validates_attachment :image, 
+						  content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
 	
 end
